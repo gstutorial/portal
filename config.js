7cf144b886731f330322770933b7e5924e778349
gstutorial <gobind.bngn@gmail.com>
2026-05-29 16:34:37 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+// Global Configuration File
+const GS_CONFIG = {
+    API_URL: 'https://script.google.com/macros/s/AKfycbwsJ-GD2wQt0F9C_Mu3cex9dy30VD6fQp2Yws81R2ICcxUXE_NIt_9ERr1Pe2PxvzbBiA/exec'
+};
