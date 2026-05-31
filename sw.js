2a9785d422e47cd93eabb6cf9452fdefabd00c9c
gstutorial <gobind.bngn@gmail.com>
2026-05-31 22:20:31 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,7 @@
-// Force the new service worker to activate immediately upon installation
-self.addEventListener('install', (event) => {
-    self.skipWaiting(); 
+self.addEventListener('install', () => {
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-    event.waitUntil(clients.claim());
-});
-
-// Intercept all network requests and force them to bypass the cache
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        fetch(event.request, { cache: "no-store" }).catch(() => {
-            return new Response("Offline. Please check your internet connection.");
-        })
-    );
+  event.waitUntil(clients.claim());
 });
